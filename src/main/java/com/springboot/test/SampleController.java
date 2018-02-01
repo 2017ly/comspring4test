ffbc07c9a5c6891fad4449bf2ab1ce90f7e5f3de
leiyu <leichuanhu@126.com>
2018-02-01 16:31:05 +0800

    springboot新增

--- src/main/java/com/springboot/test/SampleController.java
+++ src/main/java/com/springboot/test/SampleController.java
@@ -1,7 +1,12 @@
 package com.springboot.test;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +22,10 @@ public class SampleController {
     }
 
     public static void main(String[] args) throws Exception {
+       /* SpringApplication application = new SpringApplication(SampleController.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);*/
+
         SpringApplication.run(SampleController.class, args);
     }
 }
